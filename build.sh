8ae9ba400c47e77f6d0966368a4922a83df66d98
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2016-02-06 15:18:48 +0100

    Followup to last commit.

--- build.sh
+++ build.sh
@@ -6,22 +6,22 @@ unset AR ARFLAGS CC CFLAGS CXX CXXFLAGS LD LDFLAGS;
 check_prereqs git make openssl sed tar wget;
 log_msg info "Build started by ${USER}@$(hostname).";
 #trap
-(set -o errexit; mkdir -p "${PREFIX}" "${WORKDIR}");
+(set -o errexit; mkdir -p ${PREFIX} ${WORKDIR});
 BUILD_NFINI=${BUILD_NSKIP:=${BUILD_NFAIL:=${_nbuild:=0}}};
 for BUILD_LVL in 0 1 2; do
 	for BUILD_SCRIPT_FNAME in ${BUILD_LVL}[0-9][0-9].*.build; do
 		if [ -n "${DEBUG_SCRIPT}" ]\
 		&& [ "x${DEBUG_SCRIPT}" != "x${BUILD_SCRIPT_FNAME}" ]; then
 			continue;
-		elif [ ! -f "${BUILD_SCRIPT_FNAME}" ]; then
+		elif [ ! -f ${BUILD_SCRIPT_FNAME} ]; then
 			continue;
 		else
 			unset BUILD_SCRIPT_RC; : $((_nbuild+=1));
 			log_msg info "Invoking build script \`${BUILD_SCRIPT_FNAME}'";
-			(set -o errexit -- $(split "." "${BUILD_SCRIPT_FNAME%.build}");	\
-			 SCRIPT_FNAME="${BUILD_SCRIPT_FNAME}"; _pwd="$(pwd)";		\
-			 cd "${WORKDIR}"; . "${_pwd}/build.subr";		\
-			 . "${_pwd}/${BUILD_SCRIPT_FNAME}");
+			(set -o errexit -- $(split . ${BUILD_SCRIPT_FNAME%.build});	\
+			 SCRIPT_FNAME=${BUILD_SCRIPT_FNAME}; _pwd=$(pwd);		\
+			 cd ${WORKDIR}; . ${_pwd}/build.subr;				\
+			 . ${_pwd}/${BUILD_SCRIPT_FNAME});
 			case ${BUILD_SCRIPT_RC:=${?}} in
 			0) log_msg succ "Finished build script \`${BUILD_SCRIPT_FNAME}'.";
 				: $((BUILD_NFINI+=1)); continue; ;;
