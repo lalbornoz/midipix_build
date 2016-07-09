820be1bcb38273c0c7adc44b4a474c08273cdf61
Lucio Andrés Illanes Albornoz (arab, vxp) <l.illanes@gmx.de>
2016-07-09 14:36:05 +0000

    General cleanup; user-visible changes: -a nt{32,64}, -b {debug,release}.

--- build.sh
+++ build.sh
@@ -4,58 +4,108 @@
 
 . ./build.subr;
 
+#
+# Process command line arguments.
 while [ ${#} -gt 0 ]; do
 case ${1} in
 -c)	ARG_CLEAN=1; ;;
--i)	[ -z "${2}" ] && exec cat build.usage ||\
-	insert_build_script_link "${2}"; exit $?; ;;
--nd)	ARG_NO_DOWNLOAD=1; ;;
--pt)	ARG_PEDANTIC=1; ;;
+-t*)	ARG_TARBALL=1; [ "${1#-t.}" != "${1}" ] && TARBALL_SUFFIX=${1#-t.}; ;;
+-x)	ARG_XTRACE=1; set -o xtrace; ;;
+-a)	[ -z "${2}" ] && exec cat build.usage || ARCH="${2}"; shift; ;;
+-b)	[ -z "${2}" ] && exec cat build.usage || BUILD="${2}"; shift; ;;
+-i)	[ -z "${2}" ] && exec cat build.usage || insert_build_script_link "${2}"; exit; ;;
 -r)	[ -n "${ARG_RESTART_SCRIPT}" ] && exec cat build.usage;
-	match_any "${2}" :								\
-		&& { ARG_RESTART_SCRIPT="${2%%:*}"; ARG_RESTART_SCRIPT_AT="${2##*:}"; }	\
-		|| { ARG_RESTART_SCRIPT="${2}"; ARG_RESTART_SCRIPT_AT=ALL; };
-	shift; ;;
--t*)	[ ${ARG_TARBALL:-0} -eq 1 ] && exec cat build.usage || ARG_TARBALL=1;
-	[ "${1#-t.}" != "${1}" ] && TARBALL_SUFFIX=${1#-t.}; ;;
--x)	set -o xtrace; ;;
--X)	set -o xtrace; ARG_DEBUG_TARBALL=1; ;;
-*=*)	set_var_unsafe "$(get_prefix_lrg "${1}" =)"					\
-			"$(get_postfix "${1}" =)"; ;;
+	if [ "${2#*:*}" != "${2}" ]; then
+		ARG_RESTART_SCRIPT="${2%%:*}"; ARG_RESTART_SCRIPT_AT="${2##*:}";
+	else
+		ARG_RESTART_SCRIPT="${2}"; ARG_RESTART_SCRIPT_AT=ALL;
+	fi; shift; ;;
+*=*)	set_var_unsafe "${1%%=*}" "${1#*=}"; ;;
 *)	exec cat build.usage; ;;
 esac; shift; done;
 
-[ -f ${HOME}/midipix_build.vars ] && . ${HOME}/midipix_build.vars;
-[ -f ../midipix_build.vars ] && . ../midipix_build.vars;
-. ./build.vars;
-clear_env_with_except ${CLEAR_ENV_VARS_EXCEPT};
-check_path_vars ${CHECK_PATH_VARS};
-check_prereq_cmds ${CHECK_PREREQ_CMDS};
-check_prereq_files ${CHECK_PREREQ_FILES};
-if ! [ -d ${PREFIX} ]; then
-	mkdir ${PREFIX};
+# Source the build variables file and its local overrides, if any.
+for __ in ${HOME}/midipix_build.vars ../midipix_build.vars ./build.vars; do
+	[ -e ${__} ] && . ${__};
+done;
+
+#
+# Clear the environment.
+# Check whether the pathnames in build.vars contain non-empty valid values.
+# Check whether all prerequisite command names resolve.
+# Check whether all prerequisite pathnames resolve.
+for __ in $(export | sed -e 's/^export //' -e 's/=.*$//'); do
+	if ! match_list "${CLEAR_ENV_VARS_EXCEPT}" " " "${__}"; then
+		unset "${__}";
+	fi;
+done;
+for __ in ${CHECK_PATH_VARS}; do
+	if [ -z "${___:=$(get_var_unsafe "${__}")}" ]; then
+		log_msg failexit "Error: variable \`${__}' is empty or unset.";
+	elif [ "${___#* *}" != "${___}" ]; then
+		log_msg failexit "Error: variable \`${__}' contains one or more whitespace characters.";
+	fi;
+done;
+for __ in ${CHECK_PREREQ_CMDS} ${CHECK_PREREQ_FILES}; do
+	if [ "${__#/}" != "${__}" ]; then
+		if [ ! -e "${__}" ]; then
+			log_msg fail "Error: missing prerequisite file \`${__}'.";
+			__exit=1;
+		fi;
+	else
+		if ! test_cmd "${__}"; then
+			log_msg fail "Error: missing prerequisite command \`${__}'.";
+			__exit=1;
+		fi;
+	fi;
+done;
+if [ ${__exit:-0} = 1 ]; then
+	exit 1;
+elif [ -n "${__exit}" ]; then
+	unset __exit;
 fi;
+
+# Clean ${PREFIX} if requested.
+if [ ${ARG_CLEAN:-0} -eq 1 ]; then
+	log_msg info "-c specified, cleaning prefix...";
+	for __ in ${CLEAR_PREFIX_DIRS}; do
+		if [ -e ${PREFIX}/${__} ]; then
+			rm -rf -- ${PREFIX}/${__};
+		fi;
+	done;
+fi;
+
+# Create directory hierarchy and usr -> . symlinks.
+mkdir -p -- ${PREFIX} ${PREFIX_NATIVE} ${PREFIX_CROSS} ${PREFIX_TARGET}/lib ${DLCACHEDIR} ${WORKDIR};
+for __ in ${PREFIX}/usr ${PREFIX_NATIVE}/usr; do
+	if [ ! -L "${__}" ]; then
+		rm -rf -- "${__}"; ln -sf -- . "${__}";
+	fi;
+done;
+
+if [ -e ${BUILD_LOG_FNAME} ]; then
+	mv -- ${BUILD_LOG_FNAME} ${BUILD_LOG_LAST_FNAME};
+fi;
+touch ${BUILD_STATUS_IN_PROGRESS_FNAME};
+trap "rm -f ${BUILD_STATUS_IN_PROGRESS_FNAME}; log_msg failexit \"Build aborted.\"" HUP INT TERM USR1 USR2;
+
 {(
-update_build_status build_start; build_times_init; trap "clean_build_status abort; exit 1" HUP INT TERM USR1 USR2;
-log_msg info "Build started by ${BUILD_USER:=${USER}}@${BUILD_HNAME:=$(hostname)} at ${BUILD_DATE_START}.";
-log_env_vars "build (global)" ${LOG_ENV_VARS}; [ ${ARG_CLEAN:-0} -eq 1 ] && clean_prefix;
-mkdir -p ${PREFIX} ${PREFIX_NATIVE} ${PREFIX_TARGET} ${PREFIX_TARGET}/lib ${WORKDIR};
-if [ -d ${PREFIX}/usr -o -f ${PREFIX}/usr -o -L ${PREFIX}/usr ]; then
-	rm -rf ${PREFIX}/usr;
-fi;
-ln -sf . ${PREFIX}/usr;
-if [ -d ${PREFIX_NATIVE}/usr -o -f ${PREFIX_NATIVE}/usr -o -L ${PREFIX_NATIVE}/usr ]; then
-	rm -rf ${PREFIX_NATIVE}/usr;
-fi;
-ln -sf . ${PREFIX_NATIVE}/usr;
+trap "rm -f ${BUILD_STATUS_IN_PROGRESS_FNAME}; log_msg failexit \"Build aborted.\"" HUP INT TERM USR1 USR2;
+BUILD_DATE_START="$(date %Y-%m-%d-%H-%M-%S)";
 BUILD_NFINI=${BUILD_NSKIP:=${BUILD_NFAIL:=${BUILD_NBUILT:=0}}};
+BUILD_TIMES_SECS=$(command date +%s);
+log_msg info "Build started by ${BUILD_USER:=${USER}}@${BUILD_HNAME:=$(hostname)} at ${BUILD_DATE_START}.";
+log_env_vars "build (global)" ${LOG_ENV_VARS};
+
 for BUILD_LVL in 0 1 2 3 ${ARG_TARBALL:+9}; do
 	for BUILD_SCRIPT_FNAME in ${BUILD_LVL}[0-9][0-9].*.build; do
 		if [ -n "${ARG_RESTART_SCRIPT}" ]					\
 		&& [ "${ARG_RESTART_SCRIPT}" != "ALL" ]					\
 		&& ! match_list "${ARG_RESTART_SCRIPT}"					\
 				, "${BUILD_SCRIPT_FNAME}"; then
-			log_msg info "Skipped build script \`${BUILD_SCRIPT_FNAME}' (--build-scripts policy.)";
+			if [ ${ARG_XTRACE:-0} -eq 0 ]; then
+				log_msg info "Skipped build script \`${BUILD_SCRIPT_FNAME}' (--build-scripts policy.)";
+			fi;
 			continue;
 		elif [ ! -f "${BUILD_SCRIPT_FNAME}" ]; then
 			log_msg info "Build script \`${BUILD_SCRIPT_FNAME}' non-existent or not a file.";
@@ -68,40 +118,23 @@ for BUILD_LVL in 0 1 2 3 ${ARG_TARBALL:+9}; do
 					: $((BUILD_NSKIP+=1)); BUILD_SCRIPT_RC=0; continue;
 			fi;
 			log_msg info "Invoking build script${ARG_RESTART_SCRIPT:+ (forcibly)} \`${BUILD_SCRIPT_FNAME}'${ARG_RESTART_SCRIPT_AT:+ at build step ${ARG_RESTART_SCRIPT_AT}}.";
-			(set -o errexit -- $(split . ${BUILD_SCRIPT_FNAME%%.build*});	\
+			(set -o errexit -o noglob;					\
+			 set -- $(split . ${BUILD_SCRIPT_FNAME%%.build*});		\
 			 SCRIPT_FNAME=${BUILD_SCRIPT_FNAME};				\
 			 SCRIPT_NAME=${SCRIPT_FNAME%%.build*};				\
-			 export PREFIX_LVL="$(eval echo \${PREFIX_LVL${BUILD_LVL}})";	\
+			 export PKG_BUILD=${BUILD};					\
 			 export PKG_TARGET=${TARGET};					\
+			 export PKG_PREFIX=$(get_var_unsafe PKG_LVL${BUILD_LVL}_PREFIX);\
 			 export MIDIPIX_BUILD_PWD=$(pwd); cd ${WORKDIR};		\
 			 for SCRIPT_SOURCE in build.subr ${SCRIPT_NAME}.vars		\
 					${BUILD_SCRIPT_FNAME}; do			\
 			 	[ -f ${MIDIPIX_BUILD_PWD}/${SCRIPT_SOURCE} ] &&		\
-					 . ${MIDIPIX_BUILD_PWD}/${SCRIPT_SOURCE};		\
+					 . ${MIDIPIX_BUILD_PWD}/${SCRIPT_SOURCE};	\
 			 done);
 			case ${BUILD_SCRIPT_RC:=${?}} in
 			0) log_msg succ "Finished build script \`${BUILD_SCRIPT_FNAME}'.";
 				: $((BUILD_NFINI+=1)); continue; ;;
 			*) log_msg fail "Build failed in build script \`${BUILD_SCRIPT_FNAME}' (last return code ${BUILD_SCRIPT_RC}.).";
-				if [ ${ARG_DEBUG_TARBALL:-0} -eq 1 ]; then
-					log_msg info "-X specified, creating debug tarball.";
-					SCRIPT_NAME=${BUILD_SCRIPT_FNAME%%.build};
-					SCRIPT_NAME=${SCRIPT_NAME#*.};
-					SCRIPT_NAME=$(echo "${SCRIPT_NAME}" | tr a-z A-Z);
-					if [ -z "${PKG_SUBDIR=$(get_var_unsafe PKG_${SCRIPT_NAME}_SUBDIR)}" ]; then
-						PKG_URL=$(get_var_unsafe PKG_${SCRIPT_NAME}_URL);
-						PKG_FNAME=${PKG_URL##*/};
-					 	PKG_SUBDIR=${PKG_FNAME%%.tar*};
-					fi;
-					BUILD_DEBUG_TARBALL_FNAME=${PREFIX}/midipix-debug-${BUILD_USER}@${BUILD_HNAME}_$(date %Y-%m-%d-%H-%M-%S).tar.xz;
-					tar -C ${PREFIX} -cJp				\
-						-f ${BUILD_DEBUG_TARBALL_FNAME}		\
-						build.log $(cd ${PREFIX} &&		\
-							find ${WORKDIR#${PREFIX}/}	\
-								-mindepth 1 -maxdepth 1	\
-								-type d -iname ${PKG_SUBDIR}-*);
-					log_msg info "Please upload ${BUILD_DEBUG_TARBALL_FNAME} and provide an URL to it in <irc://irc.freenode.net/midipix>.";
-				fi;
 				: $((BUILD_NFAIL+=1)); break; ;;
 			esac;
 		fi;
@@ -110,10 +143,19 @@ for BUILD_LVL in 0 1 2 3 ${ARG_TARBALL:+9}; do
 		break;
 	fi;
 done;
+
 log_msg info "${BUILD_NFINI} finished, ${BUILD_NSKIP} skipped, and ${BUILD_NFAIL} failed builds in ${BUILD_NBUILT} build script(s).";
-build_times_get; log_msg info "Build time: ${BUILD_TIMES_HOURS} hour(s), ${BUILD_TIMES_MINUTES} minute(s), and ${BUILD_TIMES_SECS} second(s).";
-update_build_status finish; clean_build_status;
-exit ${BUILD_SCRIPT_RC})} 2>&1 | tee ${PREFIX}/${BUILD_LOG_FNAME:=build-$(date ${TIMESTAMP_FMT_STATUS_FILES}).log} &
+: $((BUILD_TIMES_SECS=$(command date +%s)-${BUILD_TIMES_SECS}));
+: $((BUILD_TIMES_HOURS=${BUILD_TIMES_SECS}/3600));
+: $((BUILD_TIMES_MINUTES=(${BUILD_TIMES_SECS}%3600)/60));
+: $((BUILD_TIMES_SECS=(${BUILD_TIMES_SECS}%3600)%60));
+log_msg info "Build time: ${BUILD_TIMES_HOURS} hour(s), ${BUILD_TIMES_MINUTES} minute(s), and ${BUILD_TIMES_SECS} second(s).";
+
+if [ -f "${BUILD_STATUS_IN_PROGRESS_FNAME}" ]; then
+	rm -f -- ${BUILD_STATUS_IN_PROGRESS_FNAME};
+fi;
+
+exit ${BUILD_SCRIPT_RC})} 2>&1 | tee ${BUILD_LOG_FNAME} &
 trap "kill -INT $!" HUP INT TERM USR1 USR2; wait;
 
 # vim:filetype=sh
