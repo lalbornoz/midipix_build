44046a5a791e742ef22b052b1fe2ca5e32748fe2
Lucio Andrés Illanes Albornoz (arab, vxp) <l.illanes@gmx.de>
2016-11-18 01:15:20 +0100

    subr/check_updates.subr: preliminary support for infounzip, infozip, libgpg_error, libpthread_stubs, lua, mksh, rxvt_unicode, the_silver_searcher, util_linux, and xcb_proto,

--- build.sh
+++ build.sh
@@ -51,7 +51,9 @@ check_paths; clean_prefix; create_dirs;
 init_build_log; init_build_progress_file;
 {(init_build_vars;
 log_msg info "Build started by ${BUILD_USER:=${USER}}@${BUILD_HNAME:=$(hostname)} at ${BUILD_DATE_START}.";
-log_env_vars "build (global)" ${LOG_ENV_VARS};
+if [ ${ARG_CHECK_UPDATES:-0} -eq 0 ]; then
+	log_env_vars "build (global)" ${LOG_ENV_VARS};
+fi;
 for BUILD_TARGET_LC in $(subst_tgts ${BUILD_TARGETS_META}); do
 	BUILD_TARGET=$(echo ${BUILD_TARGET_LC} | tr a-z A-Z);
 	for BUILD_PACKAGE_LC in $(get_var_unsafe ${BUILD_TARGET}_PACKAGES); do
