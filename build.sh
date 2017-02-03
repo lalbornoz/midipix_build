7cc84ba314fd750fbf4d3c39aa28db43d01652e0
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2017-02-03 18:07:44 +0100

    build.sh, etc/build.usage, {subr,vars}/*: general cleanup.

--- build.sh
+++ build.sh
@@ -3,21 +3,15 @@
 #
 
 #
-# Source subroutine scripts.
-# Process command line arguments.
+#
 #
 for __ in subr/*.subr; do
 	. ./${__};
 done;
-if [ -z "${BUILD_CPUS}" ]	\
-&& [ -e /proc/cpuinfo ]; then
-	BUILD_CPUS=$(awk '/^processor/{cpus++} END{print cpus}' /proc/cpuinfo);
-fi;
 while [ ${#} -gt 0 ]; do
 case ${1} in
 -c)	ARG_CLEAN=1; ;;
 -C)	ARG_CHECK_UPDATES=1; ;;
--n)	ARG_DRYRUN=1 ARG_VERBOSE=1; ;;
 -N)	ARG_OFFLINE=1; ;;
 -i)	ARG_IGNORE_SHA256SUMS=1; ;;
 -t*)	ARG_TARBALL=1; [ "${1#-t.}" != "${1}" ] && TARBALL_SUFFIX=${1#-t.}; ;;
@@ -43,15 +37,12 @@ esac; shift;
 done;
 
 #
-# Source the build variables file and its local overrides, if any.
-# Clear environment.
-# Check whether the pathnames in build.vars contain non-empty valid values.
-# Check whether all prerequisite command names resolve.
-# Check whether all prerequisite pathnames resolve.
-# Check whether all prerequisite Perl modules exist.
-# Clean ${PREFIX} if requested.
-# Create directory hierarchy and usr -> . symlinks.
 #
+#
+if [ -z "${BUILD_CPUS}" ]\
+&& [ -e /proc/cpuinfo ]; then
+	BUILD_CPUS=$(awk '/^processor/{cpus++} END{print cpus}' /proc/cpuinfo);
+fi;
 for __ in ${HOME}/midipix_build.vars ../midipix_build.vars ./vars/build.vars; do
 	[ -e ${__} ] && . ${__};
 done;
@@ -60,32 +51,38 @@ for __ in $(export | sed -e 's/^export //' -e 's/=.*$//'); do
 		unset "${__}";
 	fi;
 done;
-if [ -z "${BUILD_TARGETS_META}" ]; then
-	BUILD_TARGETS_META="invariants world";
-else
-	BUILD_TARGETS_META="invariants ${BUILD_TARGETS_META}";
-fi;
+
+#
+#
+#
 pre_prereqs;
 pre_subdirs;
 if [ -e ${BUILD_LOG_FNAME} ]; then
 	mv -- ${BUILD_LOG_FNAME} ${BUILD_LOG_LAST_FNAME};
 fi;
 touch ${BUILD_STATUS_IN_PROGRESS_FNAME};
-
-{(
 BUILD_DATE_START="$(date %Y-%m-%d-%H-%M-%S)";
 BUILD_NFINI=${BUILD_NSKIP:=${BUILD_NFAIL:=${BUILD_NBUILT:=0}}};
 BUILD_TIMES_SECS=$(command date +%s);
+
+#
+#
+#
+{(
 if [ ${ARG_CHECK_UPDATES:-0} -eq 0 ]; then
 	log_msg info "Build started by ${BUILD_USER:=${USER}}@${BUILD_HNAME:=$(hostname)} at ${BUILD_DATE_START}.";
 	log_env_vars "build (global)" ${LOG_ENV_VARS};
 else
 	log_msg info "Version check run started by ${BUILD_USER:=${USER}}@${BUILD_HNAME:=$(hostname)} at ${BUILD_DATE_START}.";
 fi;
-for BUILD_TARGET_LC in $(subst_tgts ${BUILD_TARGETS_META}); do
+for BUILD_TARGET_LC in $(subst_tgts invariants ${BUILD_TARGETS_META:-world}); do
 	BUILD_TARGET=$(echo ${BUILD_TARGET_LC} | tr a-z A-Z);
 	for BUILD_PACKAGE_LC in $(get_var_unsafe ${BUILD_TARGET}_PACKAGES); do
 		BUILD_PACKAGE=$(echo ${BUILD_PACKAGE_LC} | tr a-z A-Z);
+
+		#
+		#
+		#
 		if [ "${BUILD_TARGET}" != "INVARIANTS" ]\
 		&& [ -n "${ARG_RESTART}" ]; then
 			if [ "${ARG_RESTART}" != "ALL" ] &&\
@@ -96,17 +93,10 @@ for BUILD_TARGET_LC in $(subst_tgts ${BUILD_TARGETS_META}); do
 		fi;
 		if [ ${ARG_CHECK_UPDATES:-0} -eq 1 ]\
 		&& [ "${BUILD_PACKAGE#*.*}" = "${BUILD_PACKAGE}" ]; then
-			if [ ${ARG_DRYRUN:-0} -eq 1 ]; then
-				echo mode_check_pkg_updates "${BUILD_PACKAGE_LC}"		\
-					"$(get_var_unsafe PKG_${BUILD_PACKAGE}_VERSION)"	\
-					"$(get_var_unsafe PKG_${BUILD_PACKAGE}_URL)"		\
-					"$(get_var_unsafe PKG_${BUILD_PACKAGE}_URL_TYPE)";
-			else
-				mode_check_pkg_updates "${BUILD_PACKAGE_LC}"			\
-					"$(get_var_unsafe PKG_${BUILD_PACKAGE}_VERSION)"	\
-					"$(get_var_unsafe PKG_${BUILD_PACKAGE}_URL)"		\
-					"$(get_var_unsafe PKG_${BUILD_PACKAGE}_URL_TYPE)";
-			fi;
+			mode_check_pkg_updates "${BUILD_PACKAGE_LC}"			\
+				"$(get_var_unsafe PKG_${BUILD_PACKAGE}_VERSION)"	\
+				"$(get_var_unsafe PKG_${BUILD_PACKAGE}_URL)"		\
+				"$(get_var_unsafe PKG_${BUILD_PACKAGE}_URL_TYPE)";
 			continue;
 		fi;
 		if [ "${BUILD_TARGET}" != "INVARIANTS" ]\
@@ -114,50 +104,34 @@ for BUILD_TARGET_LC in $(subst_tgts ${BUILD_TARGETS_META}); do
 		&& is_build_script_done finish "${BUILD_PACKAGE_LC}"; then
 			log_msg vnfo "Skipped \`${BUILD_PACKAGE_LC}' (already built.)";
 			: $((BUILD_NSKIP+=1)); BUILD_SCRIPT_RC=0; continue;
-		fi;
-		if [ -n "${ARG_RESTART}" ]; then
+		elif [ -n "${ARG_RESTART}" ]; then
 			log_msg vnfo "Forcing package \`${BUILD_PACKAGE_LC}'.";
 		fi;
+
+		#
+		#
+		#
 		(set -o errexit -o noglob;
-		MIDIPIX_BUILD_PWD=$(pwd);
-		PKG_BUILD=${BUILD};
 		PKG_PREFIX=$(get_vars_unsafe ${BUILD_TARGET}_PREFIX PKG_${BUILD_PACKAGE%%.*}_PREFIX);				
-		PKG_TARGET=${TARGET};
-		cd ${WORKDIR};
-		for SCRIPT_SOURCE in vars/${BUILD_PACKAGE_LC%.*}.vars; do
-			if [ -f ${MIDIPIX_BUILD_PWD}/${SCRIPT_SOURCE} ]; then
-				if [ ${ARG_DRYRUN:-0} -eq 1 ]; then
-					echo . ${MIDIPIX_BUILD_PWD}/${SCRIPT_SOURCE};
-				else
-					. ${MIDIPIX_BUILD_PWD}/${SCRIPT_SOURCE};
-				fi;
-			fi;
-		done;
 		parse_with_pkg_name ${BUILD_PACKAGE_LC%.*};
-		for __ in all disabled fetch extract build_dir patch_pre autoconf patch		\
-				setup_env distclean configure clean build install; do
-			case ${__} in
-			all)
-				if test_cmd pkg_${PKG_NAME}_all; then
-					pkg_${PKG_NAME}_all; exit 0;
-				fi;
-				;;
-			disabled|build_dir|setup)
-				pkg_${__};
-				;;
-			*)	if ! is_build_script_done ${__}; then
-					if test_cmd pkg_${PKG_NAME}_${__}; then
-						pkg_${PKG_NAME}_${__};
+		for __ in ${BUILD_STEPS}; do
+			case ${__#*:} in
+			abstract)
+				if test_cmd pkg_${PKG_NAME}_${__%:*}; then
+					pkg_${PKG_NAME}_${__%:*}; exit 0;
+				fi; ;;
+			always)	pkg_${__%:*}; ;;
+			main)	if ! is_build_script_done ${__%:*}; then
+					if test_cmd pkg_${PKG_NAME}_${__%:*}; then
+						pkg_${PKG_NAME}_${__%:*};
 					else
-						pkg_${__};
+						pkg_${__%:*};
 					fi;
-				fi;
-				;;
+				fi; ;;
 			esac;
 		done;
-		set_build_script_done finish;
-		);
-		BUILD_SCRIPT_RC=${?}; case ${BUILD_SCRIPT_RC} in
+		set_build_script_done finish); BUILD_SCRIPT_RC=${?};
+		case ${BUILD_SCRIPT_RC} in
 		0) log_msg succ "Finished \`${BUILD_PACKAGE_LC}' build.";
 			: $((BUILD_NFINI+=1)); continue; ;;
 		*) log_msg fail "Build failed in \`${BUILD_PACKAGE_LC}' (last return code ${BUILD_SCRIPT_RC}.).";
@@ -168,9 +142,7 @@ for BUILD_TARGET_LC in $(subst_tgts ${BUILD_TARGETS_META}); do
 		break;
 	fi;
 done;
-for __ in copy_etc strip tarballs; do
-	post_${__};
-done;
+post_copy_etc; post_strip; post_tarballs;
 : $((BUILD_TIMES_SECS=$(command date +%s)-${BUILD_TIMES_SECS}));
 : $((BUILD_TIMES_HOURS=${BUILD_TIMES_SECS}/3600));
 : $((BUILD_TIMES_MINUTES=(${BUILD_TIMES_SECS}%3600)/60));
@@ -178,9 +150,13 @@ done;
 log_msg info "${BUILD_NFINI} finished, ${BUILD_NSKIP} skipped, and ${BUILD_NFAIL} failed builds in ${BUILD_NBUILT} build script(s).";
 log_msg info "Build time: ${BUILD_TIMES_HOURS} hour(s), ${BUILD_TIMES_MINUTES} minute(s), and ${BUILD_TIMES_SECS} second(s).";
 if [ -f "${BUILD_STATUS_IN_PROGRESS_FNAME}" ]; then
-	secure_rm ${BUILD_STATUS_IN_PROGRESS_FNAME};
+	build_fileop rm ${BUILD_STATUS_IN_PROGRESS_FNAME};
 fi;
 exit ${BUILD_SCRIPT_RC})} 2>&1 | tee ${BUILD_LOG_FNAME} &
+
+#
+#
+#
 TEE_PID=${!};
 trap "rm -f ${BUILD_STATUS_IN_PROGRESS_FNAME};	\
 	log_msg fail \"Build aborted.\";	\
