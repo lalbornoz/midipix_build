c15032d8d195d26846a278c363af8cb19a0ec471
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2019-01-19 02:36:57 +0000

    build.sh, etc/build.usage, subr/*: merges {-[ptz],--rpm} into -D {minipix,pkg,rpm,t{bz2,gz,xz},zipdist}. subr/{build_args,pkg_clean}.subr: cleanup ${ARG_CLEAN_BUILDS} usage. vars/build.vars:dist: merged into invariants. vars/invariants_minipix.vars: adapted from vars/dist_minipix_dist.vars. vars/{invariants_tarballs,midipix}.vars: don't produce source tarballs.

--- build.sh
+++ build.sh
@@ -4,21 +4,21 @@
 
 buildp_dispatch() {
 	local _msg="${1}" _pkg_name="${2}" _tgt_name="${3}"					\
-		_build_tgt_meta="" _build_tgt_lc="" _build_tgt_pkg_names="" _build_tgt_uc="";
+		_build_tgt_meta="" _build_tgt_lc="" _build_tgt_pkg_names="" _build_tgt_uc="" _build_tgts_lc="";
 	case "${_msg}" in
 	# Top-level
 	start_build)	shift; build_args "${@}"; build_init; build_checks;
 			ex_rtl_log_set_vnfo_lvl "${ARG_VERBOSE:-0}";
 			ex_rtl_log_msg info "Build started by ${BUILD_USER:=${USER}}@${BUILD_HNAME:=$(hostname)} at ${BUILD_DATE_START}.";
 			ex_rtl_log_env_vars "build (global)" ${DEFAULT_LOG_ENV_VARS};
+			_build_tgts_lc="${BUILD_TARGETS:-${TARGETS_DEFAULT}}";
+			if ! ex_rtl_lmatch "${ARG_DIST}" , rpm; then
+				_build_tgts_lc="$(ex_rtl_lfilter_not "${_build_tgts_lc}" "host_tools_rpm")";
+			fi;
 			if [ -n "${ARG_RESTART}" ]\
 			&& [ "${ARG_RESTART}" != ALL ]; then
 				_build_tgt_pkg_names="";
-				for _build_tgt_lc in ${BUILD_TARGETS:-${TARGETS_DEFAULT}}; do
-					if [ "${_build_tgt_lc}" = "host_tools_rpm" ]		\
-					&& [ "${ARG_RPM:-0}" -eq 0 ]; then
-						continue;
-					fi;
+				for _build_tgt_lc in ${_build_tgts_lc}; do
 					_build_tgt_uc="$(ex_rtl_toupper "${_build_tgt_lc}")";
 					_build_tgt_pkg_names="${_build_tgt_pkg_names:+${_build_tgt_pkg_names} }$(ex_rtl_get_var_unsafe ${_build_tgt_uc}_PACKAGES)";
 				done;
@@ -27,11 +27,7 @@ buildp_dispatch() {
 					ex_rtl_log_msg failexit "Error: package(s) \`${_build_tgt_pkg_names}' unknown.";
 				fi;
 			fi;
-			for _build_tgt_lc in ${BUILD_TARGETS:-${TARGETS_DEFAULT}}; do
-				if [ "${_build_tgt_lc}" = "host_tools_rpm" ]			\
-				&& [ "${ARG_RPM:-0}" -eq 0 ]; then
-					continue;
-				fi;
+			for _build_tgt_lc in ${_build_tgts_lc}; do
 				ex_pkg_dispatch "${_build_tgt_lc}"				\
 						"${ARG_RESTART}" "${ARG_RESTART_AT}"		\
 						buildp_dispatch;
