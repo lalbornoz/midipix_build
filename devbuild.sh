3db27f49c7b7e8d00517ba9fbcb2a957c5d04ee4
midipix <writeonce@midipix.org>
2020-06-10 09:12:32 +0000

    groups/120.native_runtime.group: added toksvc. groups/800.minipix.group: added toksvc. devbuild.sh: ditto.

--- devbuild.sh
+++ devbuild.sh
@@ -5,8 +5,8 @@ set -eu
 DEV_PACKAGES=\
 musl_no_complex_host,musl_full_host,musl_full,\
 psxtypes,pemagine,dalist,ldso,ntcon,ntapi,\
-psxscl,psxscl_strace,ntctty,ntux,ptycon,u16ports,\
-ntctty_minipix,ntux_minipix,ptycon_minipix
+psxscl,psxscl_strace,ntctty,ntux,ptycon,toksvc,u16ports,\
+ntctty_minipix,ntux_minipix,ptycon_minipix,toksvc_minipix
 
 mb_project_dir=$(cd "$(dirname $0)" ; pwd)
 cd "$mb_project_dir"
