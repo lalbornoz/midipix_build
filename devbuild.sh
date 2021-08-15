0771b516473a03c2a38607a6f6103b50827a3875
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2021-08-15 09:49:16 +0200

    devbuild.sh: updated.

--- devbuild.sh
+++ devbuild.sh
@@ -23,8 +23,8 @@ devbuild_midipix() {
 };
 
 devbuild_all() {
-	./build.sh -a nt64 -b release -D minipix,rpm,zipdist -F ipv4 -p 6 -v &&\
-	./build.sh -a nt64 -b debug -D minipix,rpm,zipdist -F ipv4 -p 6 -v;
+	./build.sh -a nt64 -b release -D minipix,rpm,zipdist -F ipv4 -p -v &&\
+	./build.sh -a nt64 -b debug -D minipix,rpm,zipdist -F ipv4 -p -v;
 };
 
 case "${1:-}" in
